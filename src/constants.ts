a902f160f75c570ee8cd201bab103c8fc54504da
Ghyath <ghyath.darwish@gmail.com>
2022-07-03 15:39:16 +0300

    Update app version

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@
 // export const SERVER_URL = 'https://2906-185-187-131-211.ngrok.io'
 export const SERVER_URL = 'https://learen.herokuapp.com'
 
-export const APP_VERSION = '2.1.1'
+export const APP_VERSION = '2.2.0'
